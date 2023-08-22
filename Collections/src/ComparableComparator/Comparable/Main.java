79b97e36cc1844fa1f6575e847dff0d1fd1ed5e3
josehigor27 <josehigor27@gmail.com>
2023-08-22 15:23:26 -0300

    feat: Comparable

---
+++ Collections/src/ComparableComparator/Comparable/Main.java
@@ -0,0 +1,30 @@
+package ComparableComparator.Comparable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Livro> livros = new ArrayList<>(5);
+        livros.add(new Livro(1L,"Ladrão de Raios",2005));
+        livros.add(new Livro(3L,"Maldição do Titã",2007));
+        livros.add(new Livro(5L,"O último olimpiano",2009));
+        livros.add(new Livro(2L,"Mar de Monstros",2006));
+        livros.add(new Livro(4L,"Batalha do labirinto",2008));
+
+
+        for(Livro livro: livros){
+            System.out.println(livro.toString());
+        }
+        System.out.println("----------------------------");
+        Collections.sort(livros);
+
+        for(Livro livro: livros){
+            System.out.println(livro.toString());
+        }
+
+    }
+}
